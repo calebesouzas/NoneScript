55b2e45f1d9f6992826a8e2fd3441e52b8e2aaed
calebesouzas <cacalebehs@gmail.com>
2025-12-24 15:12:48 -0300

    Delete parser code

--- Include/parser.h
+++
@@ -1,7 +0,0 @@
-#ifndef NONE_PARSER_H
-#define NONE_PARSER_H
-#include "lexer.h"
-
-void parse(TokenArray* tokenArray);
-
-#endif /* NONE_PARSER_H */
--- Source/parser.c
+++
@@ -1,9 +0,0 @@
-#include <stdio.h>
-#include "lexer.h"
-#include "parser.h"
-
-void parse(TokenArray* tokenArray) {
-  for (int i = 0; i < tokenArray->count; i++) {
-    printf("Found token: %s\n", tokenArray->tokens[i].value);
-  }
-}
