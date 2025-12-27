76ed9ce2ec0d7c79e59f7b99ef21ca25008c4d3c
Calebe <swplayermc30@gmail.com>
2025-12-27 18:11:33 -0300

    feat(error-handling): remove 'details' parameter from 'panic()'

--- Source/errors.h
+++ Source/errors.h
@@ -1,15 +1,6 @@
 #ifndef ERRORS_H
 #define ERRORS_H
 
-
-typedef struct {
-  enum {
-    Ok,
-    Error
-  } status;
-  char error[32];
-} Result;
-
-void panic(const char *local, const char *reason, const char **details);
+void panic(const char *local, const char *reason);
 
 #endif /* ERRORS_H */
