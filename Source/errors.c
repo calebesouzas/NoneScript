cf662c0977a6689fec1d2d90a057391d622b2e83
Calebe <swplayermc30@gmail.com>
2025-12-27 18:11:33 -0300

    feat(error-handling): remove 'details' parameter from 'panic()'

--- Source/errors.c
+++ Source/errors.c
@@ -1,16 +1,9 @@
 #include "errors.h"
 
 #include <stdio.h>
+#include <stdlib.h>
 
-void panic(const char *local, const char *reason, const char **details) {
+void panic(const char *local, const char *reason) {
   (void)printf("Panicked at '%s'\nReason: %s\n", local, reason);
-
-  if (details != NULL) {
-    unsigned int i = 0;
-    puts("Details:\n");
-    do {
-      (void)printf("\t%s\n", details[i]);
-      ++i;
-    } while (details[i] != NULL);
-  }
+  exit(1);
 }
